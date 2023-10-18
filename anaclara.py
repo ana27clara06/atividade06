0703d4b3d51603ddd1ed281d6f5f0e496c19ea3b
ana27clara06 <4n4clara@gmail.com>
2023-10-18 11:24:20 -0300

    first commit

---
+++ anaclara.py
@@ -0,0 +1,18 @@
+numero = int(input("digite um numero inteiro"))
+t1 = numero * 1
+t2 = numero * 2
+t3 = numero * 3
+t4 = numero * 4
+t5 = numero * 5
+t6 = numero * 6
+t7 = numero * 7
+t8 = numero * 8
+print(f"""{t1}
+{t2}
+{t3}
+{t4}
+{t5}
+{t6}
+{t7}
+{t8}
+""")
